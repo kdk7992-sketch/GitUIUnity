fbbade49ebb391bd4c5018abb75023244075439e
김동관 <kdk7992@gmail.com>
2025-11-25 22:27:02 +0900

    feat: stage8,9해결

--- Assets/Scripts/UI/PopupBank.cs
+++ Assets/Scripts/UI/PopupBank.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 
@@ -5,13 +6,19 @@ public class PopupBank : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject depositMenu;
-    public GameObject withdrawMenu; 
+    public GameObject withdrawMenu;
+    public TMP_InputField depositInputField;
+    public TMP_InputField withdrawInputField;
+    public GameObject popupError;
+    public UIManager uiManager;
 
+    
     public void OnClickDepositMenu()
     {
         mainMenu.SetActive(false);
         depositMenu.SetActive(true);
         withdrawMenu.SetActive(false);
+        depositInputField.text = "";
     }
 
     public void OnClickWithDrawMenu()
@@ -19,6 +26,7 @@ public class PopupBank : MonoBehaviour
         mainMenu.SetActive(false);
         withdrawMenu.SetActive(true);
         depositMenu.SetActive(false);
+        withdrawInputField.text = "";
     }
 
     public void OnClickDepositBack()
@@ -32,4 +40,49 @@ public class PopupBank : MonoBehaviour
         mainMenu.SetActive(true);
         withdrawMenu.SetActive(false);
     }
+
+    public void OnClickDeposit(int amount)
+    {
+        UserData data = GameManager.Instance.userdata;
+
+        if (data.Cash < amount)
+        {
+            popupError.SetActive(true);
+            return;
+        }
+        data.Cash -= amount;
+        data.Balance += amount;
+
+        uiManager.Refresh(data);
+    }
+
+    public void OnClickDepositInput()
+    {
+        if (string.IsNullOrEmpty(depositInputField.text)) return; 
+        OnClickDeposit(int.Parse(depositInputField.text));
+    }
+    public void OnClickWithDraw(int amount)
+    {
+        UserData data = GameManager.Instance.userdata;
+
+        if (data.Balance < amount)
+        {
+            popupError.SetActive(true);
+            return;
+        }
+        data.Cash += amount;
+        data.Balance -= amount;
+
+        uiManager.Refresh(data);
+    }
+    public void OnClickWithDrawInput()
+    {
+        if (string.IsNullOrEmpty(withdrawInputField.text)) return; 
+        OnClickWithDraw(int.Parse( withdrawInputField.text));
+    }
+    public void OnClickErrorOK()
+    {
+        popupError.SetActive(false);
+    }
+    
 }
